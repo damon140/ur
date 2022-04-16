c885ac9b27ac7ed1c234de15ee453c8faa050569
Damon van Opdorp <damon@damon140.com>
2022-04-16 21:15:25 +1000

    Improve printing

---
+++ src/main/java/com/damon140/ur/DrawnBoard.java
@@ -0,0 +1,7 @@
+package com.damon140.ur;
+
+public class DrawnBoard {
+
+
+
+}
--- src/main/java/com/damon140/ur/Game.java
+++ src/main/java/com/damon140/ur/Game.java
@@ -1,17 +1,19 @@
 package com.damon140.ur;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Game {
 
-
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        System.out.println("Hello world!!");
+        System.out.println("Command line Ur");
+        System.out.println();
         new Game().run();
     }
 
@@ -27,42 +29,62 @@ public class Game {
     }
 
     public void run() {
-
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            // need loop
-            System.out.println(board.horizontalFullBoardStrings().stream().collect(Collectors.joining("\r\n")));
-            System.out.println(board.currentTeam() + "'s turn");
-            System.out.println("");
-
             int roll = dice.roll();
-            System.out.println("Roll is: " + roll);
+
+            List<String> gameLines = board.horizontalFullBoardStrings().stream().collect(Collectors.toCollection(ArrayList::new));
 
             AtomicInteger index = new AtomicInteger(1);
             Map<Board.Square, Board.Square> moves = ur.askMoves(board.currentTeam(), roll);
-            String moveDesc = moves.entrySet()
+
+            List<String> instructionLines = new ArrayList<>();
+            instructionLines.add(board.currentTeam() + "'s turn roll is: " + roll);
+            moves.entrySet()
                     .stream()
                     .map(entry -> index.getAndIncrement() + " " + entry.getKey() + " -> " + entry.getValue())
-                    .collect(Collectors.joining("\r\n"));
-            System.out.println(moveDesc);
-            System.out.println("x - quit ");
-            System.out.println("");
-            System.out.println("input: ");
-            String input = scanner.nextLine();
+                    .collect(Collectors.toCollection(() -> instructionLines));
 
-            System.out.println("got " + input);
+            instructionLines.add("x - quit ");
+            instructionLines.add("");
+
+            int maxGameLineLength =  gameLines.stream().mapToInt(s -> s.length()).max().getAsInt();
+
+            String gameTurn = IntStream.range(0, Math.max(gameLines.size(), instructionLines.size()))
+                    .mapToObj(i -> {
+                        String gamePart = getIfPresentOrPad(gameLines, i, maxGameLineLength);
+                        String instructionPart = getIfPresentOrPad(instructionLines, i, maxGameLineLength);
+
+                        return gamePart + "      " + instructionPart;
+                    })
+                    .collect(Collectors.joining("\r\n"));
+            System.out.println(gameTurn);
+
+            if (0 == roll) {
+                continue; // skip turn
+            }
+
+            System.out.print("input: ");
+            String input = scanner.next();
+            //System.out.println("got " + input);
 
             if ("x".equals(input)) {
                 System.out.println("Done!!");
                 return;
             }
 
-            int moveIndex = Integer.parseInt(input.trim());
+            int moveIndex = Integer.parseInt(input);
             Board.Square fromSquare = moves.keySet().stream().toList().get(moveIndex - 1);
 
             ur.moveCounter(fromSquare, roll);
         }
+    }
+
+    private String getIfPresentOrPad(List<String> arr, int i, int padLength) {
+        return i < arr.size()
+                ? arr.get(i)
+                : " ".repeat(padLength);
     }
 
 }
