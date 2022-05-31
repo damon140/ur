f2d88efe18020918114207db459acbdf5d32e5b4
Damon van Opdorp <damon@damon140.com>
2022-05-31 23:18:10 +1000

    draw circles

--- hello2/src/main/kotlin/web/UrCanvasView.kt
+++ hello2/src/main/kotlin/web/UrCanvasView.kt
@@ -1,6 +1,7 @@
 package web
 
 import org.w3c.dom.CanvasRenderingContext2D
+import kotlin.math.PI
 
 class UrCanvasView(pageObject: UrPageObject) {
     private val pageObject: UrPageObject
@@ -16,11 +17,28 @@ class UrCanvasView(pageObject: UrPageObject) {
         // Super mega overworked example here, is not very easy to follow, too OO, shapes encapsulate canvas
         // https://play.kotlinlang.org/byExample/09_Kotlin_JS/05_Canvas
 
-        var ctx: CanvasRenderingContext2D = c.getContext("2d") as CanvasRenderingContext2D
+        var canvas: CanvasRenderingContext2D = c.getContext("2d") as CanvasRenderingContext2D
 
-        ctx.beginPath();
-        ctx.rect(20.0, 20.0, 150.0, 100.0);
-        ctx.stroke();
+        canvas.beginPath();
+        canvas.rect(20.0, 20.0, 150.0, 100.0);
+        canvas.stroke();
+
+        // empty circle
+        canvas.beginPath();
+        canvas.arc(100.0, 75.0, 50.0, 0.0, 2 * PI);
+        canvas.lineWidth = 3.0
+        canvas.stroke();
+
+        // filled circle
+        canvas.beginPath();
+        canvas.arc(130.0, 100.0, 50.0, 0.0, 2 * PI);
+        canvas.lineWidth = 3.0
+        canvas.fill();
+        // how in Kotlin?
+        //canvas.fillStyle = 'green';
+        canvas.stroke();
+
+
     }
 
 }
