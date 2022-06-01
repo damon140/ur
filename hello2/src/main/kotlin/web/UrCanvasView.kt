b244e6c781a96fd6efb2b716ee5ab07dadf0acdf
Damon van Opdorp <damon@damon140.com>
2022-06-01 20:44:28 +1000

    Adds starter click function.

--- hello2/src/main/kotlin/web/UrCanvasView.kt
+++ hello2/src/main/kotlin/web/UrCanvasView.kt
@@ -1,6 +1,8 @@
 package web
 
 import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.MouseEvent
 import kotlin.math.PI
 
 class UrCanvasView(pageObject: UrPageObject) {
@@ -10,14 +12,13 @@ class UrCanvasView(pageObject: UrPageObject) {
         this.pageObject = pageObject
     }
 
-    // TODO: move to new class
     fun drawSquare() {
-        var c = pageObject.findCanvasBoard()
+        var htmlCanvasElement = pageObject.findCanvasBoard()
 
         // Super mega overworked example here, is not very easy to follow, too OO, shapes encapsulate canvas
         // https://play.kotlinlang.org/byExample/09_Kotlin_JS/05_Canvas
 
-        var canvas: CanvasRenderingContext2D = c.getContext("2d") as CanvasRenderingContext2D
+        var canvas: CanvasRenderingContext2D = htmlCanvasElement.getContext("2d") as CanvasRenderingContext2D
 
         canvas.beginPath();
         canvas.rect(20.0, 20.0, 150.0, 100.0);
@@ -38,7 +39,11 @@ class UrCanvasView(pageObject: UrPageObject) {
         //canvas.fillStyle = 'green';
         canvas.stroke();
 
-
+        htmlCanvasElement.onclick = { e: MouseEvent ->
+            console.log("x: " + e.clientX);
+            console.log("y: " + e.clientY);
+            this
+        }
     }
 
 }
