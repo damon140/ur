c5236eb096271f69bb01aa7430f3cd9aa24ff049
Damon van Opdorp <damon@damon140.com>
2022-06-19 16:58:19 +1000

    game loop planning.

---
+++ hello2/src/main/kotlin/web/GameLoop.kt
@@ -0,0 +1,32 @@
+package web
+
+class GameLoop {
+
+    // private stepLastRun
+
+    // m01 draw
+    // m02 wait for roll button to be clicked *
+    // m03 roll button clicked <- button handler
+    //
+    // first cut??
+    //
+    // m04 draw (hide roll button)
+    // m05 play roll sound
+    // m06 wait for roll noise to finish *
+    // m07 rollNoiseFinished <- interval
+    // m08 generate new roll
+    // m09 calculate moves
+    // m10 draw (show roll and show moves)
+    // m11 wait for move or skipped from player *
+    // m12 move selection received <- button handler or fn
+    // m13 play counter move sound of wah-wah 1 2 3 4
+    // m14 wait for move sound to finish *
+    // m15 move sound finished <- interval
+    // m16 move
+    // m17 draw here??
+    // m18 play counter taken or off board sound if needed
+    // m19 wait for sound to finish *
+    // m20 counter sound finished <- interval
+    // m21 change player if needed
+
+}
