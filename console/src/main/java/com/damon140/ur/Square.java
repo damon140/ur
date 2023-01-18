99aab686bbf52a98d214265ea09ed8912a71baa2
Damon van Opdorp <damon@damon140.com>
2023-01-18 21:35:28 +1100

    Adding license.

--- console/src/main/java/com/damon140/ur/Square.java
+++ console/src/main/java/com/damon140/ur/Square.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2023 Damon van Opdorp
+ *
+ * Licensed under GNU General Public License v3.0.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.damon140.ur;
 
 import java.util.Optional;
